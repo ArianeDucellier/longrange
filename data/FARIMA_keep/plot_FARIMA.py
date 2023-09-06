c5f5b6b3e663f9de0fcf987230f77a4087ef9d53
ArianeDucellier <ariane.ducellier@gmail.com>
2023-09-06 10:08:06 -0700

    Finish demo notebook

--- data/FARIMA_keep/plot_FARIMA.py
+++ data/FARIMA_keep/plot_FARIMA.py
@@ -11,44 +11,47 @@ import pickle
 from datetime import datetime, timedelta
 from math import floor
 
-files = ['series_1_1', 'series_1_2', 'series_1_3', 'series_1_4', 'series_1_5', \
-         'series_2_1', 'series_2_2', 'series_2_3', 'series_2_4', 'series_2_5', \
-         'series_3_1', 'series_3_2', 'series_3_3', 'series_3_4', 'series_3_5', \
-         'series_4_1', 'series_4_2', 'series_4_3', 'series_4_4', 'series_4_5', \
-         'series_5_1', 'series_5_2', 'series_5_3', 'series_5_4', 'series_5_5', \
-         'series_6_1', 'series_6_2', 'series_6_3', 'series_6_4', 'series_6_5', \
-         'series_7a_1', 'series_7a_2', 'series_7a_3', 'series_7a_4', \
-         'series_7b_1', 'series_7b_2', \
-         'series_8a_1', 'series_8a_2', 'series_8a_3', 'series_8a_4', \
-         'series_8b_1', 'series_8b_2', \
-         'series_9_1', 'series_9_2', 'series_9_3', 'series_9_4', \
-         'series_10_1', 'series_10_2', 'series_10_3', 'series_10_4']
+#files = ['series_1_1', 'series_1_2', 'series_1_3', 'series_1_4', 'series_1_5', \
+#         'series_2_1', 'series_2_2', 'series_2_3', 'series_2_4', 'series_2_5', \
+#         'series_3_1', 'series_3_2', 'series_3_3', 'series_3_4', 'series_3_5', \
+#         'series_4_1', 'series_4_2', 'series_4_3', 'series_4_4', 'series_4_5', \
+#         'series_5_1', 'series_5_2', 'series_5_3', 'series_5_4', 'series_5_5', \
+#         'series_6_1', 'series_6_2', 'series_6_3', 'series_6_4', 'series_6_5', \
+#         'series_7a_1', 'series_7a_2', 'series_7a_3', 'series_7a_4', \
+#         'series_7b_1', 'series_7b_2', \
+#         'series_8a_1', 'series_8a_2', 'series_8a_3', 'series_8a_4', \
+#         'series_8b_1', 'series_8b_2', \
+#         'series_9_1', 'series_9_2', 'series_9_3', 'series_9_4', \
+#         'series_10_1', 'series_10_2', 'series_10_3', 'series_10_4']
+files = ['series_9_2']
 
-innovations = ['Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', \
-               'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', \
-               'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', \
-               'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', \
-               'Exponential', 'Exponential', 'Exponental', 'Exponential', 'Exponential', \
-               'Lognormal', 'Lognormal', 'Lognormal', 'Lognormal', 'Lognormal', \
-               'Symmetic stable', 'Symmetic stable', 'Symmetic stable', 'Symmetic stable', \
-               'Symmetic stable', 'Symmetic stable', \
-               'Symmetic stable', 'Symmetic stable', 'Symmetic stable', 'Symmetic stable', \
-               'Symmetic stable', 'Symmetic stable', \
-               'Pareto', 'Pareto', 'Pareto', 'Pareto', \
-               'Skewed stable', 'Skewed stable', 'Skewed stable', 'Skewed stable']
+#innovations = ['Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', \
+#               'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', \
+#               'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', \
+#               'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', 'Gaussian', \
+#               'Exponential', 'Exponential', 'Exponental', 'Exponential', 'Exponential', \
+ #              'Lognormal', 'Lognormal', 'Lognormal', 'Lognormal', 'Lognormal', \
+ #              'Symmetic stable', 'Symmetic stable', 'Symmetic stable', 'Symmetic stable', \
+ #              'Symmetic stable', 'Symmetic stable', \
+ #              'Symmetic stable', 'Symmetic stable', 'Symmetic stable', 'Symmetic stable', \
+ #              'Symmetic stable', 'Symmetic stable', \
+ #              'Pareto', 'Pareto', 'Pareto', 'Pareto', \
+ #              'Skewed stable', 'Skewed stable', 'Skewed stable', 'Skewed stable']
+innovations = ['Pareto']
 
-true_d = [0.0, 0.1, 0.2, 0.3, 0.4, \
-          0.0, 0.1, 0.2, 0.3, 0.4, \
-          0.0, 0.1, 0.2, 0.3, 0.4, \
-          0.0, 0.1, 0.2, 0.3, 0.4, \
-          0.0, 0.1, 0.2, 0.3, 0.4, \
-          0.0, 0.1, 0.2, 0.3, 0.4, \
-          0.0, 0.1, 0.2, 0.3, \
-          0.0, 0.1, \
-          0.0, 0.1, 0.2, 0.3, \
-          0.0, 0.1, \
-          0.0, 0.1, 0.2, 0.3, \
-          0.0, 0.1, 0.2, 0.3]
+#true_d = [0.0, 0.1, 0.2, 0.3, 0.4, \
+#          0.0, 0.1, 0.2, 0.3, 0.4, \
+#          0.0, 0.1, 0.2, 0.3, 0.4, \
+#          0.0, 0.1, 0.2, 0.3, 0.4, \
+#          0.0, 0.1, 0.2, 0.3, 0.4, \
+#          0.0, 0.1, 0.2, 0.3, 0.4, \
+#          0.0, 0.1, 0.2, 0.3, \
+#          0.0, 0.1, \
+#          0.0, 0.1, 0.2, 0.3, \
+#          0.0, 0.1, \
+#          0.0, 0.1, 0.2, 0.3, \
+#          0.0, 0.1, 0.2, 0.3]
+true_d = [0.1]
 
 params = {'legend.fontsize': 24, \
           'xtick.labelsize':24, \
@@ -67,5 +70,6 @@ for i in range(0, len(files)):
     plt.title('FARIMA time series with {} innovations and fractional parameter = {}'. \
         format(innovations[i], true_d[i]), fontsize=24)
     plt.tight_layout()
-    plt.savefig(files[i] + '.eps', format='eps')
+#    plt.savefig(files[i] + '.eps', format='eps')
+    plt.savefig(files[i] + '.png', format='png')
     plt.close(1)
